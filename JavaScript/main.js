ad8e9d52670e9ece52a90f6831e1b48b6f1cf73e
ulises-david <ulisesdavidacosta27@gmail.com>
2021-12-08 13:58:50 -0300

    areglar problema links

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,11 +1,21 @@
-const menu = document.querySelector('.menu')
-const menuLista = document.querySelector('.ul_bg')
-const cerrarLista = document.querySelector('.cerrar')
+const menu = document.querySelector('.menu');
+const menuLista = document.querySelector('.ul_bg');
+const cerrarLista = document.querySelector('.cerrar');
+const linkAbrir = document.querySelector('#link');
+const blur = document.querySelector('.destacados_container')
+const tarjetas = document.querySelector('.carta')
+console.log();
 
 menu.addEventListener('click', () =>{
-    menuLista.classList.add('active')
-})
+    menuLista.classList.add('active');
+});
 
 cerrarLista.addEventListener('click', () =>{
-    menuLista.classList.remove('active')
+    menuLista.classList.remove('active');
+});
+
+linkAbrir.addEventListener('click', () => {
+    blur.classList.add('blurActive')
+    tarjetas.classList.add('tarjeta')
+
 })
